acfc3e820f0ec6feb374e0a6d5e46fc1528e1796
cdai6 <103866552+cdai6@users.noreply.github.com>
2022-04-17 10:17:20 +0800

    Session 1 Prompt for Code Journal 1.

---
+++ cj1-1.py
@@ -0,0 +1,20 @@
+def print_name(name):
+    print(f'My name is {name}.')
+
+def print_pronouns(pron):
+    print(f'My preferred pronouns is {pron}.')
+
+def print_mv(moive):
+    print(f'My favourite movie is {moive}.')
+
+def print_fd(food):
+    print(f'My favourite food is {food}.')
+
+if __name__ == '__main__':
+    print_name('Chuanxin Dai')
+    print_pronouns('he/him/his')
+    print_mv('Forrest Gump')
+    print_fd('taco')
+
+
+
